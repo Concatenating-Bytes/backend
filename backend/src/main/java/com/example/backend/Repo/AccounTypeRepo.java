b07321dcb68d630ee3d98ee8ab29225ee187f7ef
Sourasish Biswas <sourasishbiswas7@gmail.com>
2025-06-30 23:05:37 +0530

    added acount repo

--- backend/src/main/java/com/example/backend/Repo/AccounTypeRepo.java
+++ backend/src/main/java/com/example/backend/Repo/AccounTypeRepo.java
@@ -1,4 +1,9 @@
 package com.example.backend.Repo;
 
-public interface AccounTypeRepo {
+import com.example.backend.Entity.AccountType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccounTypeRepo extends JpaRepository<AccountType,Long> {
 }
